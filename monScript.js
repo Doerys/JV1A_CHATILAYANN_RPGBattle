b77e087bd7ecef572f4b6b579b881f3586b3656c
Doerys <yann.chatila@hotmail.fr>
2022-12-01 13:26:29 +0100

    Avancées inutiles

--- monScript.js
+++ monScript.js
@@ -12,39 +12,39 @@ animationMonstre1 = document.getElementById("animationMonstre1");
 boutonAttaque.onclick = function() {
 
     contenuBoiteDialogue.innerHTML = "L'archer tire !";
-    animationHero1.setAttribute("src", "images/attackPersonnage1.gif");
+    animationHero1.setAttribute("src", "images/attackArcher.gif");
 
     //dégâts attaque
 
     setTimeout(function() {
         vieMonstre1.innerHTML = parseInt(vieMonstre1.innerHTML) - 5;
-        animationHero1.setAttribute("src", "images/idlePersonnage1.gif");
+        animationHero1.setAttribute("src", "images/idleArcher.gif");
         contenuBoiteDialogue.innerHTML = "Le monstre reçoit 5 dégâts !";
-        animationMonstre1.setAttribute("src", "images/hitMonstre1.gif");}, 850);
+        animationMonstre1.setAttribute("src", "images/hitSpectre.gif");}, 850);
         
     //animation monstre Idle
 
     setTimeout(function() {
-        animationMonstre1.setAttribute("src", "images/idleMonstre1.gif");}, 1350);
+        animationMonstre1.setAttribute("src", "images/idleSpectre.gif");}, 1350);
     
     //riposte du monstre
 
     setTimeout(function() {
-        animationMonstre1.setAttribute("src", "images/attackMonstre1.gif");
+        animationMonstre1.setAttribute("src", "images/attackSpectre.gif");
         contenuBoiteDialogue.innerHTML = "Le monstre riposte !"}, 2000);
 
     //dégâts du monstre
     
     setTimeout(function() {
         vieHero1.innerHTML = parseInt(vieHero1.innerHTML) - 5;
-        animationMonstre1.setAttribute("src", "images/idleMonstre1.gif");
+        animationMonstre1.setAttribute("src", "images/idleSpectre.gif");
         contenuBoiteDialogue.innerHTML = "Vous subissez 5 dégâts !";
-        animationHero1.setAttribute("src", "images/hitPersonnage1.gif");}, 3500);
+        animationHero1.setAttribute("src", "images/hitArcher.gif");}, 3500);
 
     //retour à la normale
 
     setTimeout(function() {
-        animationHero1.setAttribute("src", "images/idlePersonnage1.gif");}, 3950);
+        animationHero1.setAttribute("src", "images/idleArcher.gif");}, 3950);
 
     tour += 1;
             
